b8aea0327c989efeb556a7374cebec50a8c00de7
Lera <miss_val@bk.ru>
2022-11-28 10:24:55 +0300

    Задача 4

---
+++ 2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int M=b;
+if (a >= b)
+{
+    M = a;
+}
+if (c >= b)
+{
+    M = c;
+}
+Console.Write("Наибольшее число: " + M);
