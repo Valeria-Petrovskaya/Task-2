bbccd326b8a95444a22d8a0fae8d2a77295bc951
Lera <miss_val@bk.ru>
2022-11-28 07:44:03 +0300

    Задача 2

---
+++ 1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > b ? "Число a больше числа b": "Число b больше чичсла a");
+
